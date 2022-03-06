434654ff3272082f05cf4cea8e102918ff2edd92
FantoZz <yevgeniif@gmail.com>
2022-03-06 17:06:40 +0200

    create bd systems, its work

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # my_cashflow
+bd locate in my_cashflow\bd_mycashflow
+
+format project.txt:
+1 line = name
+2 line = category
+3 line = description
+4 line = cashflow
+5 line = percent complete
--- bdcore.py
+++ bdcore.py
@@ -35,5 +35,45 @@ def editCashflow(id,cashflow):
     with open(file, "w") as f:
         f.write(str(cashflow))
 
-def getallproject(id):
+class projectC:
+    def __init__(self,name,category,description,percent_complete):
+        self.name = name
+        self.category = category
+        self.description = description
+        self.percent_complete = percent_complete
+
+def getAllprojects(id):
+    file = "bd_mycashflow\\"
+    file += id + "\\projects.txt"
+    check_file = os.path.exists(file)
+    if check_file == 0:
+        retext="У вас нет проектов"
+    else:
+        projects = []
+        i=0
+        j=0
+        projects[j] = projectC()
+        with open(file, "r") as f:
+            for line in f:
+                match i:
+                    case 0:
+                        projects[j].name=line
+                    case 1:
+                        projects[j].category = line
+                    case 2:
+                        projects[j].description = line
+                    case 3:
+                        projects[j].cashflow = line
+                    case 4:
+                        projects[j].percent_complete=line
+                if i>4:
+                    i-=4
+                    j+=1
+                    projects[j]=projectC()
+                i+=1
+                #formation retext
+                print(line)
     return 0
+
+def editproject(id,number):
+    a=1
